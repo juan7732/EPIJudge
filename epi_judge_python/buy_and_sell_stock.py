5b6bed7310bc0412439c6936072f2e596038efa5
Juan Ruiz Valencia <juan7732@gmail.com>
2023-09-01 18:49:45 -0500

    solutions and problem mapping

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -2,10 +2,18 @@ from typing import List
 
 from test_framework import generic_test
 
+# [310, 315, 275, 295, 260, 270, 290, 230, 255, 250]
 
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    min_price = float('inf')
+    max_profit = 0.0
+    for i in range(len(prices)):
+        if prices[i] < min_price:
+            min_price = prices[i]
+            continue
+        if prices[i] - min_price > max_profit:
+            max_profit = prices[i] - min_price
+    return max_profit
 
 
 if __name__ == '__main__':
